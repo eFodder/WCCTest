d0fc1a696cc04b52e1eec42c2c7e5087c0b6e465
eFodder <rudi.rademacher@gmail.com>
2015-01-09 12:01:24 +1300

    test

--- js/index.js
+++ js/index.js
@@ -34,8 +34,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		
-		alert('on ready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
--- js/js_main.js
+++ js/js_main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('.content_inner div').click(function(){
 		$('.content_inner').each(function(){
 			$(this).css('color','#000');
@@ -12,4 +12,4 @@ $(document).ready(function(){
 		alert('test alert');
 		
 	});	
-})
+})*/
