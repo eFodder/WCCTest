2ae280c20ac3608f9fb29ab9e84c6e76071edf17
eFodder <rudi.rademacher@gmail.com>
2015-01-09 12:52:45 +1300

    test

--- js/js_main.js
+++ js/js_main.js
@@ -1,9 +1,10 @@
 function populateDeviceDetails(){
-	$('#content_outer .device_details').HTML('Device Model: '    + device.model    + '<br />' +
-                            'Device Cordova: '  + device.cordova  + '<br />' +
-                            'Device Platform: ' + device.platform + '<br />' +
-                            'Device UUID: '     + device.uuid     + '<br />' +
-                            'Device Version: '  + device.version  + '<br />');
+	alert('init');
+	$('#content_outer .device_details').html('Device Model: ' + device.model + '<br />' +
+		'Device Cordova: '  + device.cordova  + '<br />' +
+		'Device Platform: ' + device.platform + '<br />' +
+		'Device UUID: '     + device.uuid     + '<br />' +
+		'Device Version: '  + device.version  + '<br />');
 }
 
 function fireDeviceReady(){
