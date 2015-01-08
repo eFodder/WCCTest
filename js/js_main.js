422844cb6387bea784fde1e2ace78aeef0503065
eFodder <rudi.rademacher@gmail.com>
2015-01-08 15:44:26 +1300

    Commit with initial phonegap site build

---
+++ js/js_main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.content_inner div').click(function(){
+		$('.content_inner').each(function(){
+			$(this).css('color','#000');
+		});
+		
+		$(this).parent().css('color','#fff');
+		
+		var thisColour = $(this).parent().css('background-color');
+		$('#head_outer h1').css('color',thisColour);
+	});
+})
