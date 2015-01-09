2d28c27836f232877a878df09f283d23851fd83c
eFodder <rudi.rademacher@gmail.com>
2015-01-09 14:10:00 +1300

    location

--- js/js_main.js
+++ js/js_main.js
@@ -68,6 +68,30 @@ function onFail(message) {
   alert('Failed because: ' + message);
 }
 
+function getCurrentPosition() {
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+// onSuccess Geolocation
+function onSuccess(position) {
+	$('#location-output').html(
+		'Latitude: '           + position.coords.latitude              + '<br />' +
+		'Longitude: '          + position.coords.longitude             + '<br />' +
+		'Altitude: '           + position.coords.altitude              + '<br />' +
+		'Accuracy: '           + position.coords.accuracy              + '<br />' +
+		'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+		'Heading: '            + position.coords.heading               + '<br />' +
+		'Speed: '              + position.coords.speed                 + '<br />' +
+		'Timestamp: '          + position.timestamp                    + '<br />');
+}
+
+// onError Callback receives a PositionError object
+function onError(error) {
+	$('#location-output').html('Unable to obtain position:<br>' + 
+		'code: '    + error.code    + '<br>' +
+		'message: ' + error.message + '<br>');
+}
+
 function fireDeviceReady(){
 	//Setup photo capture variables
 	pictureSource=navigator.camera.PictureSourceType;
